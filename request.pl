6673548c758c12055d455765a9835cdb4efee6ee
root <root@dpi.gorinfo.net>
2014-05-08 11:52:25 +0400

    modified:   request.pl

--- request.pl
+++ request.pl
@@ -11,6 +11,9 @@ use utf8;
 
 binmode(STDOUT,':utf8');
 
+my $pin = '111111';
+my $dn = 'ProviderDN';
+
 my $dt = DateTime->now();
 $dt->set_time_zone( 'Europe/Moscow' );
 
@@ -24,13 +27,18 @@ my %request = (
 		"email" => "EMAIL",
 );
 
+open my $fh, '>:encoding(windows-1251)', './request.xml' || die "open(request.xml): $!";
+
 my $xs = new XML::Simple(
 	NoAttr => 1,
 	RootName => "request",
 	XMLDecl => '<?xml version="1.0" encoding="windows-1251"?>',
+	OutputFile => $fh,
 );
 my $xml = $xs->XMLout(\%request);
 
-print $xml;
+system('/opt/cprocsp/bin/amd64/cryptcp -signf ./request.xml -cert -pin '. $pin	.' -dn ' . $dn .' -nochain -norev');
+
+#print $xml;
 
 #print("TIME: " . $dat . "\n");
